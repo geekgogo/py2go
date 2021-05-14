d08de181d8dd295b0f0aeab8de19b0336ed7e5a6
geekgogo <detail.ed@outlook.com>
2021-05-14 11:35:45 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# 使用golang重写python小脚本
+
---
+++ sushu.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+	a := []int{1}
+	for i:=0; i<=100; i++ {
+		if i > 1{
+			num := i / 2
+			for {
+				if num > 1{
+					if i % num == 0{
+						break
+					}else{
+						num --
+					}
+				}else{
+					a = append(a, i)
+					break
+				}
+			}
+		}
+	}
+	fmt.Println(a)
+}
